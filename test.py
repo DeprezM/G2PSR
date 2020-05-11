fdf0d469c6b8db7660e36061381b09df6908feef
@jumoreir <Moreirafabrizy.julien@gmail.com>
2020-05-11 14:30:27 +0200

    added testing of the new architecture

--- test.py
+++ test.py
@@ -11,22 +11,26 @@ import torch as pt
 import numpy as np
 
 #parameters
-samplesize=10
-q=5
+samplesize=15
+q=10
 g=3
-z=3
+z=5
 
 #creating dummy data
-X=pt.rand(samplesize,g,requires_grad=True)
+X=pt.randn(samplesize,g,requires_grad=True)
 temp=np.random.rand(g,q)
 temp[2]=[0] * q
 temp=temp.astype(float)
 Y=pt.tensor(temp, requires_grad=True, dtype=pt.float)
 Z=X@Y
 
-test=a.VDAutoencoder(Z.shape, z).to(a.DEVICE)
+# test=a.VDAutoencoder(Z.shape, z).to(a.DEVICE)
 
-print(X)
-print(Z)
+# print(X)
+# print(Z)
 
-test.optimize(Z, 3000)
+# test.optimize(Z, 4000)
+
+test=a.VDonWeightAE(Z).to(a.DEVICE)
+
+test.optimize(Z, 4000)
